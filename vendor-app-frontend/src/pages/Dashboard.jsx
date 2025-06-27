9dacf70c5cc20c5e3c769c6625ab975cdebb264a
tanishkapatidar <tanishkapatidar18@gmail.com>
2025-06-27 05:56:13 +0530

    updated Dashboard and added logout button

--- vendor-app-frontend/src/pages/Dashboard.jsx
+++ vendor-app-frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React , {useState} from 'react'
 import VendorForm from '../components/VendorForm'
 import VendorList from '../components/VendorList'
 
 const Dashboard = () => {
+
+    const [selectedVendor, setSelectedVendor] = useState(null);
+
+    const logout = () => {
+        localStorage.removeItem('user');
+        window.location.href = '/';
+    };
+    
+    
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (!user) {
+        window.location.href = '/';
+        return null;
+    }
     return (
         <div style={{padding:'2rem'}}>
             <h1>Vendor Dashboard</h1>
-            <VenderForm />
-            <VendorList />
+            <p>Welcome, {user.name}</p>
+            <button onClick={logout}>Logout</button>
+            <hr />
+            <VendorForm 
+                selectedVendor={selectedVendor}
+                clearSelectedVendor={() => setSelectedVendor(null)}
+            />
+
+            <VendorList setSelectedVendor = {setSelectedVendor} />
         </div>
     );
 };
