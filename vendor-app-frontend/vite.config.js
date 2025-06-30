b8f1aa52fa95ddbca6a47ba99f77adec5cbe7e01
tanishkapatidar <tanishkapatidar18@gmail.com>
2025-06-30 23:54:48 +0530

    added FE url to allowed hosts

--- vendor-app-frontend/vite.config.js
+++ vendor-app-frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 4173, // or any port Render assigns
+    host: '0.0.0.0',
+    allowedHosts: ['vendor-app-fe.onrender.com'] // ✅ Add this line
+  }
 })
